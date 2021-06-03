9d47dc362f4414b598f8492b74918b3d4da529e3
Sahil <sahil@webmobinfo.ch>
2021-06-03 17:26:17 +0530

    any

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
 import { client } from '../client'
 
 const Home = ({ entries }) => {
-  console.log('Home ===>>> ',entries.items.length)
+  // console.log('Home ===>>> ',entries)
   return (
     <section>
-      <p>Hello World! This is Sahil's NextJS here</p>
+      <h2>Sahil's Pages</h2>
+      {
+        entries.items.map((item, idx) => {
+          return (
+            <p key={idx}>{item.fields.name}</p>
+          )
+        })
+      }
     </section>
   )
 }
 
 export const getStaticProps = async () => {
-  const entries = await client.getEntries();
+  const entries = await client.getEntries({
+    content_type: 'page'
+  });
   // const entries = await result.json();
   console.log('getStaticProps ===>>> ',entries.items.length)
   return {
