8b8fda465a4afe38c9f4c297d6f7f19b7306a4fd
Sahil <sahil@webmobinfo.ch>
2021-06-03 17:17:25 +0530

    any

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import { client } from '../client'
-import { useEffect } from 'react'
 
-export default function Home() {
-  useEffect(() => {
-    client.getEntries()
-      .then(function (entries) {
-        console.log(entries) // 200
-      })
-  })
-
+const Home = ({ entries }) => {
+  console.log('Home ===>>> ',entries.items.length)
   return (
     <section>
       <p>Hello World! This is Sahil's NextJS here</p>
     </section>
   )
 }
+
+export const getStaticProps = async () => {
+  const entries = await client.getEntries();
+  // const entries = await result.json();
+  console.log('getStaticProps ===>>> ',entries.items.length)
+  return {
+    props: { entries: entries }
+  }
+}
+
+export default Home
