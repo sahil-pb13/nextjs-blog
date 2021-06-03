0af808b65ff0b6913e517e57fa95dba3980ccb1c
Sahil <sahil@webmobinfo.ch>
2021-06-03 17:04:11 +0530

    any

---
+++ client.js
@@ -0,0 +1,6 @@
+import * as contentful from 'contentful';
+
+export const client = contentful.createClient({
+    space: 'mpajf6dp9ep9',
+    accessToken: 'apOsTU1PmYK87N6B_oZHMfswyDLThM4ljz1blbf19-s'
+})
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import { client } from '../client'
+import { useEffect } from 'react'
+
 export default function Home() {
+  useEffect(() => {
+    client.getEntries()
+      .then(function (entries) {
+        console.log(entries) // 200
+      })
+  })
+
   return (
-      <section>
-        <p>Hello World! This is Sahil's NextJS here</p>
-      </section>
+    <section>
+      <p>Hello World! This is Sahil's NextJS here</p>
+    </section>
   )
 }
